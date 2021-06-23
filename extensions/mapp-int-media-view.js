e94f7c5eb402b7de467667b22aac09674e18bb14
damiano.milani <damiano.milani@mediaset.it>
2021-06-23 15:15:13 +0200

    third_party_groupLabel and third_party_subGroupLabel on ck11

--- extensions/mapp-int-media-view.js
+++ extensions/mapp-int-media-view.js
@@ -103,6 +103,7 @@ if (a === 'view' || !(b.event_name === 'video:analytics' || b.tealium_event === 
       // Get Media Parameter
       getParameter: function(dl, event) {
         var parameter = {
+          11:[(dl.third_party_groupLabel || "mediaset"), (dl.third_party_subGroupLabel || "mediaset")].join("."),
           16: (dl.video_play_request_page_url || dl.videoPageUrl || dl.pageUrl || dl.page_url || '').replace(/^.*\/\/(.*)/, 'https://$1'),
           20: dl.video_site_section,
           22: dl.video_ad_block,
