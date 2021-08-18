7bdb0e18a537e280df21b4bc0dbaa5c35ae35afc
damiano.milani <damiano.milani@mediaset.it>
2021-08-18 12:41:29 +0200

    default_page_url typo

--- extensions/mapp-int-default-values.js
+++ extensions/mapp-int-default-values.js
@@ -4,7 +4,7 @@ b.default_aggregate = "aggregato tgcom24";
 b.default_site = "sportmediaset";
 b.default_app_name = "app//sportmediaset-app/noversion";
 b.default_content_id = "mediaset-sportmediaset-homepage-/";
-b.defaut_page_url = "https://www.sportmediaset.mediaset.it/";
+b.default_page_url = "https://www.sportmediaset.mediaset.it/";
 
 b.brand = "sportmediaset";
 b.host = "sportmediaset.mediaset.it"; //without www if is possible
